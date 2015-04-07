d316c1a15bcba3fff5e876eb586fde3ffa47e7df
bonkeyfonkey <fergusmorton@live.com>
2015-04-07 15:49:31 +0100

    Order Status and Product (Scope)
    created scope to check if the active scope is set to true

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :active, :category, :description, :manufacturer, :name, :price, :title
+  
+   has_many :order_items
+
+   default_scope { where(active: true) }
 end
