1b51d8bcf5ed0c182bad706600f38cb82023f305
bonkeyfonkey <fergusmorton@live.com>
2015-04-07 18:15:13 +0100

    Debugging

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ShoppingSystem::Application.routes.draw do
+
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+  root to: "products#index"
+  
   get "order_items/create"
 
   get "order_items/update"
@@ -9,10 +15,7 @@ ShoppingSystem::Application.routes.draw do
 
   get "products/index"
   
-  resources :products, only: [:index]
-  resource :cart, only: [:show]
-  resources :order_items, only: [:create, :update, :destroy]
-  root to: "products#index"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
