15a9da77620483775db3bfc0a7aeb311b4dfe221
bonkeyfonkey <fergusmorton@live.com>
2015-04-07 15:41:40 +0100

    Seeding Commit
    Adding fruit product

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Product.delete_all
+Product.create! id: 1, name: "Banana", price: 0.49, active: true, title: "Fruit", manufacturer: "WholeFoods", category: "Carbohydrate", description: "For sport"
+Product.create! id: 2, name: "Apple", price: 0.29, active: true, title: "Fruit", manufacturer: "WholeFoods",   category: "Fiber", description: "For teachers"
+Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true,  title: "Fruit", manufacturer: "WholeFoods", category: "Vitamin C", description: "Fresh in Summer"
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
